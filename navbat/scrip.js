7da7d033de4e798f16faa6cae4d4fa6f092c8bdd
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2024-03-02 22:54:43 +0000

    create simple, basic and not Attractive

---
+++ navbat/scrip.js
@@ -0,0 +1,7 @@
+
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".navbar");
+
+btn.addEventListener("click", function() {
+    nav.classList.toggle("nav-open");
+})
